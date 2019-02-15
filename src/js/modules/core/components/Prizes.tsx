85c7215b0e280d639addc2373fe1f52487d718f4
Kristel Fung <kristelfung@gmail.com>
2019-02-14 23:38:01 -0500

    grid commits

--- src/js/modules/core/components/Prizes.tsx
+++ src/js/modules/core/components/Prizes.tsx
@@ -3,27 +3,28 @@ import injectSheet, {WithStyles} from "react-jss";
 import { Theme } from "../../ThemeInjector";
 
 const styles = (theme: Theme) => ({
+  prizesSection: {
+    padding: "5%"
+  },
   prizes: {
     backgroundColor: theme.backgroundColor,
-    width: "100%",
-    display: "flex",
-    flexWrap: "wrap"
+    display: "grid",
+    gridTemplateColumns: "25% 25% 25% 25%",
+    gridTemplateRows: "auto auto"
   },
   prizeItem: {
-    width: "100%",
-    flex: "25%",
-    textAlign: "center",
-    flexGrow: "0",
-    flexShrink: "0"
+    maxWidth: "500px",
+    padding: "20px",
+    textAlign: "center"
   },
   [`@media(max-width: ${theme.mediumBreakpoint})`]: {
-    prizeItem: {
-      flex: "50%"
+    prizes: {
+      gridTemplateColumns: "50% 50%"
     }
   },
   [`@media(max-width: ${theme.smallBreakpoint})`]: {
-    prizeItem: {
-      flex: "100%"
+    prizes: {
+      gridTemplateColumns: "50% 50%"
     }
   }
 })
@@ -32,18 +33,36 @@ type Props = WithStyles<typeof styles>;
 
 const Prizes: React.FunctionComponent<Props> = ({ classes }) => {
   return (
-    <div className={classes.prizes}>
-      <div className={classes.prizeItem}>
-        <p>prize one</p>
-      </div>
-      <div className={classes.prizeItem}>
-        <p>prize two</p>
-      </div>
-      <div className={classes.prizeItem}>
-        <p>prize three</p>
-      </div>
-      <div className={classes.prizeItem}>
-        <p>prize four</p>
+    <div className={classes.prizesSection}>
+      <div className={classes.prizes}>
+        <div className={classes.prizeItem}>
+          <p>Sample content! Nope! HackNYU welcomes people of all skill levels. 
+            In fact, part of the fun of HackNYU is seeing 
+            beginners learn from hackathon veterans—and 
+            hopefully vice versa! We will also provide a a 
+            mentoring service to help guide new hackers.</p>
+        </div>
+        <div className={classes.prizeItem}>
+          <p>Sample content! Nope! HackNYU welcomes people of all skill levels. 
+            In fact, part of the fun of HackNYU is seeing 
+            beginners learn from hackathon veterans—and 
+            hopefully vice versa! We will also provide a a 
+            mentoring service to help guide new hackers.</p>
+        </div>
+        <div className={classes.prizeItem}>
+          <p>Sample content! Nope! HackNYU welcomes people of all skill levels. 
+            In fact, part of the fun of HackNYU is seeing 
+            beginners learn from hackathon veterans—and 
+            hopefully vice versa! We will also provide a a 
+            mentoring service to help guide new hackers.</p>
+        </div>
+        <div className={classes.prizeItem}>
+          <p>Sample content! Nope! HackNYU welcomes people of all skill levels. 
+            In fact, part of the fun of HackNYU is seeing 
+            beginners learn from hackathon veterans—and 
+            hopefully vice versa! We will also provide a a 
+            mentoring service to help guide new hackers.</p>
+        </div>
       </div>
     </div>
   )
